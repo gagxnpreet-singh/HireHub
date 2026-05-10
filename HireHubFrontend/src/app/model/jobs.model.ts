1fd7c8af1925b2874a844f362aea67ed798fb7a6
Gaganpreet Singh <69570333+gagxnpreet-singh@users.noreply.github.com>
2026-05-10 05:49:57 -0400

    Improve job model naming with backward-compatible alias

--- HireHubFrontend/src/app/model/jobs.model.ts
+++ HireHubFrontend/src/app/model/jobs.model.ts
@@ -1,11 +1,14 @@
-export class Jobs {
-    id:number | undefined;
-    title : string = '';
-    skills : string = '';
-    role : string = '';
-    type : string = '';
-    experience : string='';
-    description : string= '';
-    postedBy:any;
-    studentsApplied:any[] | undefined;
+export class Job {
+    id: number | undefined;
+    title: string = '';
+    skills: string = '';
+    role: string = '';
+    type: string = '';
+    experience: string = '';
+    description: string = '';
+    postedBy: any;
+    studentsApplied: any[] | undefined;
 }
+
+// Backward-compatible alias for existing imports.
+export class Jobs extends Job {}
